bf1471496e7bed7ebec17a9e106f56a8b3732b58
DRILLE-BAIANO <duduzimhard@gmail.com>
2025-03-10 10:44:59 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- StikerSmash/app/(tabs)/index.tsx
+++ StikerSmash/app/(tabs)/index.tsx
@@ -1,11 +1,15 @@
 import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
+import ImageViewer from '@/components/ImageViewer';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
-export default function HomeScreen() {
+export default function Index() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      
+    })
+  }
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -17,7 +21,7 @@ export default function HomeScreen() {
       }>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+        
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
